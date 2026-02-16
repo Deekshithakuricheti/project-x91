16c4665e40ee6b12bcaac50c0f2dc4928e7861f9
Deekshithakuricheti <kdeekshitha2646@gmail.com>
2026-02-16 18:20:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+const messageText = "From the moment I met you, my world became brighter. Your smile is my favorite sight. Will you be my Valentine? ❤️";
+
+let index = 0;
+const speed = 40;
+const messageElement = document.getElementById("message");
+
+// Typing effect
+function typeWriter() {
+  if (index < messageText.length) {
+    messageElement.innerHTML += messageText.charAt(index);
+    index++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+typeWriter();
+
+// No button moving
+const noBtn = document.getElementById("noBtn");
+
+noBtn.addEventListener("mouseover", function () {
+  const x = Math.random() * (window.innerWidth - 100);
+  const y = Math.random() * (window.innerHeight - 50);
+
+  noBtn.style.position = "absolute";
+  noBtn.style.left = x + "px";
+  noBtn.style.top = y + "px";
+});
+
+// Yes button with confetti
+document.getElementById("yesBtn").addEventListener("click", function () {
+
+  confetti({
+  particleCount: 120,
+  spread: 80,
+  startVelocity: 40,
+  gravity: 0.8,
+  ticks: 200,
+  origin: { y: 0.6 }
+}); 
+
+  setTimeout(function () {
+    document.body.innerHTML = `
+      <div style="
+        height:100vh;
+        display:flex;
+        justify-content:center;
+        align-items:center;
+        background:linear-gradient(135deg, #ff758c, #ff7eb3);
+        color:white;
+        text-align:center;
+        font-size:2rem;
+      ">
+        You just made me the happiest person alive 💕
+      </div>
+    `;
+  }, 1000);
+
+});
